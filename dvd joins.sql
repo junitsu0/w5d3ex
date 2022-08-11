1fa0a6f9d89ba15b6a4b0f91f1640967214219c3
Justin <junitsu00@gmail.com>
2022-08-11 09:26:45 -0500

    argh

---
+++ dvd joins.sql
@@ -0,0 +1,59 @@
+-- Question 1
+
+select customer_id
+from customer
+join address
+on address.address_id = customer.customer_id
+where district = 'Texas';
+
+
+--Answer = customer_id of 10, 122, 310, 405, 567
+------------------------------
+--Question 2
+
+select first_name, last_name, amount
+from customer
+join payment
+on customer.customer_id = payment.customer_id
+where amount > 6.99
+group by first_name, last_name, amount
+order by amount desc;
+
+--Answer = see query 992
+------------------------------
+--Question 3
+
+select first_name, last_name
+from customer;
+
+select SUM(amount), customer_id 
+from payment
+group by customer_id
+having SUM(amount) > 175
+;
+--Answer = 
+-------------------------------
+--Question 4
+
+select customer_id
+from city
+
+--Answer = 
+--------------------------------
+--Question 5
+
+SELECT *
+FROM category 
+WHERE category_id = (
+	SELECT category_id
+	FROM film_category 
+	GROUP BY category_id
+	ORDER BY COUNT(*) DESC
+	LIMIT 1
+);
+
+--Answer = Sports @ 15
+--------------------------------
+--Question 6
+
+
